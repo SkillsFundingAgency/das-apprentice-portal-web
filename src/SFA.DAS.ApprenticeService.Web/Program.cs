c17bab2e24e1abfdfaf024f6d43def3fed34c687
mld3 <57175069+MarieDale3@users.noreply.github.com>
2021-04-27 08:18:03 +0100

    CS-406 start page first draft

--- src/SFA.DAS.ApprenticeService.Web/Program.cs
+++ src/SFA.DAS.ApprenticeService.Web/Program.cs
@@ -20,7 +20,7 @@ namespace SFA.DAS.ApprenticeService.Web
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<ApplicationStartup>();
                 });
     }
 }
--- src/SFA.DAS.ApprenticeService.Web/Startup/ApplicationStartup.cs
+++ src/SFA.DAS.ApprenticeService.Web/Startup/ApplicationStartup.cs
@@ -1,19 +1,15 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace SFA.DAS.ApprenticeService.Web
 {
-    public class Startup
+    public class ApplicationStartup
     {
-        public Startup(IConfiguration configuration)
+        public ApplicationStartup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
