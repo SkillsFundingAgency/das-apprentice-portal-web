925f5adb792122fb7566be8e2e043372b55f8cf0
mld3 <57175069+MarieDale3@users.noreply.github.com>
2021-05-17 16:39:36 +0100

    CS-406 setup connection to config storage account

--- src/SFA.DAS.ApprenticeService.Web/Program.cs
+++ src/SFA.DAS.ApprenticeService.Web/Program.cs
@@ -1,5 +1,8 @@
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using NLog.Web;
+using SFA.DAS.ApprenticeCommitments.Web.Startup;
 
 namespace SFA.DAS.ApprenticeService.Web
 {
@@ -7,14 +10,14 @@ namespace SFA.DAS.ApprenticeService.Web
     {
         public static void Main(string[] args)
         {
+            NLogStartup.ConfigureNLog();
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<ApplicationStartup>();
-                });
+        public static IWebHostBuilder CreateHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureAzureTableConfiguration()
+                .UseStartup<ApplicationStartup>()
+                .UseNLog();
     }
 }
