5957c05523cd9621e5bee9b2e8c2cedd06082299
Ben Arnold <ben@arnoldfamily.me.uk>
2021-07-07 06:00:37 +0100

    Fix step-by-step navigation

--- src/SFA.DAS.ApprenticePortal.Web/wwwroot/js/site.js
+++ src/SFA.DAS.ApprenticePortal.Web/wwwroot/js/site.js
@@ -2,7 +2,7 @@
 // Step by step
 
 var $stepByStep = $('#step-by-step-navigation')
-if ($stepByStep) {
+if ($stepByStep.length > 0) {
     var stepByStepNavigation = new GOVUK.Modules.AppStepNav()
     stepByStepNavigation.start($stepByStep)
 }
