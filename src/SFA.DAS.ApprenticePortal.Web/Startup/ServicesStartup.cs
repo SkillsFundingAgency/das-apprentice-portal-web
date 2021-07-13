647378d47ffe009798b4def8e7cb8c42e2337402
Ben Arnold <ben@arnoldfamily.me.uk>
2021-07-13 07:10:53 +0100

    🔥 Removed unnecessary code

--- src/SFA.DAS.ApprenticePortal.Web/Startup/ServicesStartup.cs
+++ src/SFA.DAS.ApprenticePortal.Web/Startup/ServicesStartup.cs
@@ -1,26 +1,12 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using RestEase.HttpClientFactory;
 using SFA.DAS.ApprenticePortal.Web.Services.OuterApi;
-//using SFA.DAS.ApprenticeCommitments.Web.Services;
-//using SFA.DAS.ApprenticeCommitments.Web.Services.OuterApi;
-//using SFA.DAS.ApprenticeCommitments.Web.TagHelpers;
 using SFA.DAS.Http.Configuration;
 
 namespace SFA.DAS.ApprenticePortal.Web.Startup
 {
     public static class ServicesStartup
     {
-        //public static IServiceCollection RegisterServices(
-        //    this IServiceCollection services)
-        //{
-        //    services.AddTransient<RegistrationsService>();
-        //    services.AddTransient<AuthenticatedUserClient>();
-        //    services.AddTransient<ISimpleUrlHelper, AspNetCoreSimpleUrlHelper>();
-        //    services.AddScoped<VerifiedUserService>();
-        //    services.AddScoped<ITimeProvider, UtcTimeProvider>();
-        //    return services;
-        //}
-
         public static IServiceCollection AddOuterApi(
             this IServiceCollection services,
             OuterApiConfiguration configuration)
