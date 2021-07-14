7c3810201fbeb9c8071ae61c3ebc76ebf2d3e515
chazzysb <cballagan@yahoo.com>
2021-07-14 16:40:46 +0100

    CS-807-GovUkFooterLinks fix code smells

--- src/SFA.DAS.ApprenticePortal.Web/Pages/TermsAndConditions.cshtml.cs
+++ src/SFA.DAS.ApprenticePortal.Web/Pages/TermsAndConditions.cshtml.cs
@@ -1,14 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SFA.DAS.ApprenticePortal.Web.Pages
 {
     public class TermsAndConditionsModel : PageModel
     {
+        /// <summary>
+        /// Page yet to be implemented
+        /// </summary>
         public void OnGet()
         {
         }
