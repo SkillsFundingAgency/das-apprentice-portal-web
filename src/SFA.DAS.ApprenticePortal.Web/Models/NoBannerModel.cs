f826f3955ba6a07297f87e23d331e6e1e866a5a0
chazzysb <cballagan@yahoo.com>
2021-11-03 11:31:12 +0000

    CS-1239-HideBanner

---
+++ src/SFA.DAS.ApprenticePortal.Web/Models/NoBannerModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using SFA.DAS.ApprenticePortal.SharedUi.Menu;
+
+namespace SFA.DAS.ApprenticePortal.Web.Models
+{
+    [HideNavigationBar]
+    public class NoBannerModel : PageModel
+    {
+    }
+}
