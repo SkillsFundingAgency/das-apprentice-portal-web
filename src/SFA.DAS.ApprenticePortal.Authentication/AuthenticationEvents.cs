68c1c27b4c4009a3114e636d27d4423c840983a4
codewolf86 <chris@codewolfconsulting.com>
2022-05-16 15:03:39 +0100

    Modify Authentication Package for Beta User Claim

--- src/SFA.DAS.ApprenticePortal.Authentication/AuthenticationEvents.cs
+++ src/SFA.DAS.ApprenticePortal.Authentication/AuthenticationEvents.cs
@@ -124,6 +124,9 @@ namespace SFA.DAS.ApprenticePortal.Authentication
 
             if (apprentice.TermsOfUseAccepted)
                 principal.AddTermsOfUseAcceptedClaim();
+
+            if (apprentice.IsPrivateBetaUser)
+                principal.AddPrivateBetaUserClaim();
         }
 
         private static void UpdateApprenticeAccountClaims(ClaimsPrincipal principal, IApprenticeAccount apprentice)
