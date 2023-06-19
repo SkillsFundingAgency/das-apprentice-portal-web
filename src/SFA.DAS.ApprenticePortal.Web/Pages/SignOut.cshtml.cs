241455cb985c4cb059956753190fa17b4513df68
Deven Shah <devenshah1974@gmail.com>
2023-06-19 12:40:05 +0100

    CSP-677 Added signout page in the portal

---
+++ src/SFA.DAS.ApprenticePortal.Web/Pages/SignOut.cshtml.cs
@@ -0,0 +1,20 @@
+using System.Linq;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SFA.DAS.ApprenticePortal.Web.Pages;
+
+public class SignOutModel : PageModel
+{
+    public IActionResult OnGet()
+    {
+        foreach (var cookie in Request.Cookies.Keys.Where(x => x.Contains(".Apprenticeships.Application")))
+        {
+            Response.Cookies.Delete(cookie);
+        }
+
+        return SignOut(CookieAuthenticationDefaults.AuthenticationScheme, OpenIdConnectDefaults.AuthenticationScheme);
+    }
+}
