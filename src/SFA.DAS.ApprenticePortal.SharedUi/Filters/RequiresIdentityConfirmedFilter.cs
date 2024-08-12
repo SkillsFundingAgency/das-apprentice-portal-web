a517effb5446520c4dff7660365f85f16a3aac7b
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2024-08-12 20:19:24 +0100

    FAI-1312 - Update filter to check api if T&Cs not accepted on claim

--- src/SFA.DAS.ApprenticePortal.SharedUi/Filters/RequiresIdentityConfirmedFilter.cs
+++ src/SFA.DAS.ApprenticePortal.SharedUi/Filters/RequiresIdentityConfirmedFilter.cs
@@ -1,25 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using SFA.DAS.ApprenticePortal.SharedUi.Menu;
 
 namespace SFA.DAS.ApprenticePortal.Authentication.Filters
 {
-    public class RequiresIdentityConfirmedFilter : IAuthorizationFilter
+    public class RequiresIdentityConfirmedFilter : IAsyncAuthorizationFilter
     {
         private readonly NavigationUrlHelper _urlHelper;
         private readonly AuthenticatedUser _user;
+        private readonly IApprenticeAccountProvider _provider;
 
-        public RequiresIdentityConfirmedFilter(NavigationUrlHelper urlHelper, AuthenticatedUser user)
+        public RequiresIdentityConfirmedFilter(NavigationUrlHelper urlHelper, AuthenticatedUser user, IApprenticeAccountProvider provider)
         {
             _urlHelper = urlHelper;
             _user = user;
+            _provider = provider;
         }
-        public void OnAuthorization(AuthorizationFilterContext context)
+        public async Task OnAuthorizationAsync(AuthorizationFilterContext context)
         {
             if (!_user.HasCreatedAccount)
                 context.Result = new RedirectResult(_urlHelper.Generate(NavigationSection.ConfirmMyApprenticeship, $"register{context.HttpContext.Request.QueryString}"));
             else if (!_user.HasAcceptedTermsOfUse)
-                context.Result = new RedirectResult(_urlHelper.Generate(NavigationSection.ApprenticeAccounts, "AcceptTermsOfUse"));
+            {
+                var apprentice = await _provider.GetApprenticeAccount(_user.ApprenticeId);
+                if (!apprentice!.TermsOfUseAccepted)
+                {
+                    context.Result = new RedirectResult(_urlHelper.Generate(NavigationSection.ApprenticeAccounts, "AcceptTermsOfUse"));    
+                }
+            }
+                
         }
     }
 }
